4a08c882537cca984568cdf265fca4cc32313ce9
Erwin <erwin.paralesda@gmail.com>
2022-07-05 10:39:11 -0500

    Add files via upload

---
+++ Uilities/TimerFeatures/TimerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MySchoolAPI.Uilities.TimerFeatures
+{
+    public class TimerManager
+    {
+
+        private readonly Timer _timer;
+        private readonly AutoResetEvent _autoResetEvent;
+        private readonly Action _action;
+
+        public DateTime TimerStarted { get; set; }
+
+
+
+        public TimerManager(Action action)
+        {
+            _action = action;
+            _autoResetEvent = new AutoResetEvent(false);
+            _timer = new Timer(Execute, _autoResetEvent, 1000, 2000);
+            TimerStarted = DateTime.Now;
+        }
+
+
+
+        private void Execute(object stateInfo)
+        {
+            _action();
+
+            if ((DateTime.Now - TimerStarted).Seconds > 60)
+            {
+                _timer.Dispose();
+            }
+        }
+
+    
+    }
+}
