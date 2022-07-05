2df52903ceb382d4b2faaeeeaf7b68f91f9369f4
Erwin <erwin.paralesda@gmail.com>
2022-07-05 11:06:03 -0500

    Add files via upload

---
+++ DTOs/StudentCreationDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySchoolAPI.DTOs
+{
+    public class StudentCreationDTO
+    {
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        public string FirstName { get; set; }
+        [StringLength(maximumLength: 50)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 15)]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 50)]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 50)]
+        public string Career { get; set; }
+    }
+}
