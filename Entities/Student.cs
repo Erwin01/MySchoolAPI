91969cb1a95a4f9a00c6abdf927db47c3054ff3b
Erwin <erwin.paralesda@gmail.com>
2022-07-05 11:05:49 -0500

    Add files via upload

---
+++ Entities/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySchoolAPI.Entities
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        public string FirstName { get; set; }
+        [StringLength(maximumLength: 50)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 15)]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Field {0} is required")]
+        [StringLength(maximumLength: 50)]
+        public string Career { get; set; }
+
+    }
+}
